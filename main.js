7623815e32a2ec0021a43c42e3dc24357171d072
Samitheopps <sami.ousmaal@gmail.com>
2024-02-06 08:47:04 +0100

    Maj Site Web

---
+++ main.js
@@ -0,0 +1,8 @@
+// Fonction appelée lorsque le bouton Commander est cliqué
+function commander() {
+    alert('Votre commande a été enregistrée!');
+  }
+  
+  // Attacher la fonction au bouton Commander
+  document.getElementById('commanderButton').addEventListener('click', commander);
+  
